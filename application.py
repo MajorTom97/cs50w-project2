35297565c724a796b961ea78a0058b5ffc470155
MajorTom97 <cesargalileo1969@gmail.com>
2021-10-26 17:35:18 -0600

    Dritte Aktualisierung

--- application.py
+++ application.py
@@ -6,67 +6,48 @@ from helpers import login_required
 from time import localtime, strftime
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SECRET_KEY"] = "esta/esmi/secret-key"
 socketio = SocketIO(app)
 
+
 # global variables
-nicknames = dict()
+nicknames = []
 channels = ["general"]
 
 @app.route("/")
 def index():
-    if "nickname" in session:
+    if "nombre" in session:
         return render_template("chat.html")
     else:
         return render_template("signin.html")
 
-@app.route("/singin", methods=["GET", "POST"])
-def singin():
-    if request.method == "POST":
+@app.route("/chat", methods=["GET", "POST"])
+def chat():
+    if request.method == "GET":
+        if "nombre" in session:
+            return render_template("chat.html")
+        else:
+            return render_template("signin.html")
+    else:
+
        nickname = request.form.get("nickname")
         # Check if the nicknmane is already useb by current user or not
        if nickname in nicknames:
-           return redirect("/signin", message="Nickname already taken") 
+           return redirect("/chat", message="Nickname already taken") 
        # Session updated and add user to logged-in users
-       session['nickname'] = nickname
-       # Add an nickname with value 
-       nicknames[nickname] = None 
+       session['nombre'] = nickname
+       # Add the list the names
+       nicknames.append(nickname)
        #Initialize the general channel
        session['currentChannel'] = "general"
+       print("---------------------")
+       print(nickname)
+       print(session['nombre'])
        
-       return redirect("/chat")    
-    else: 
-        request.method == "GET"
-        return render_template("signin.html")
+       return redirect("/") 
 
 @app.route("/logout")
 def logout():
     # Remove the current user from the lists of users
-    nicknames.pop(session['nickname'], None)
-
     session.clear()
-    return redirect(url_for("base"))
-
-@app.route("/channels", methods=["GET", "POST"])
-def channels():
-    if request.method == "POST":
-        # newChannel
-        newChannel = request.form.get("room-name")
-
-        #update to the current channel
-        session['currentChannel'] = newChannel
-
-        # If it is a new channel room
-        if newChannel not in channels:
-            # Add a new channel text
-            channels.append(newChannel)
-        return redirect(url_for("chat"))
-
-    elif request.method == "GET":
-        return render_template("channels.html")
-
-@app.route("/chat")
-@login_required
-def chat():
-    # Current lists of channels as the current users of the channels
-    return render_template("chat.html", channels=channels, nickname=nicknames)
+    return redirect("chat")
