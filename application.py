194af85fb2c35d941e03f92c269d8c65998b8fcd
MajorTom97 <cesargalileo1969@gmail.com>
2021-10-26 22:32:05 -0600

    Aktualisierung 26/10

--- application.py
+++ application.py
@@ -1,12 +1,12 @@
 import os
 
 from flask import Flask, redirect, render_template, request, session, url_for
-from flask_socketio import SocketIO, emit, send, leave_room, join_room
+from flask_socketio import SocketIO, emit, send
 from helpers import login_required
 from time import localtime, strftime
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "esta/esmi/secret-key"
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY") or "esta/esmi/secret-key"
 socketio = SocketIO(app)
 
 
@@ -16,15 +16,15 @@ channels = ["general"]
 
 @app.route("/")
 def index():
-    if "nombre" in session:
-        return render_template("chat.html")
+    if "name" in session:
+        return render_template("chat.html", channels=channels)
     else:
         return render_template("signin.html")
 
 @app.route("/chat", methods=["GET", "POST"])
 def chat():
     if request.method == "GET":
-        if "nombre" in session:
+        if "name" in session:
             return render_template("chat.html")
         else:
             return render_template("signin.html")
@@ -33,16 +33,16 @@ def chat():
        nickname = request.form.get("nickname")
         # Check if the nicknmane is already useb by current user or not
        if nickname in nicknames:
-           return redirect("/chat", message="Nickname already taken") 
+           return redirect("/chat") 
        # Session updated and add user to logged-in users
-       session['nombre'] = nickname
+       session['name'] = nickname
        # Add the list the names
        nicknames.append(nickname)
        #Initialize the general channel
        session['currentChannel'] = "general"
        print("---------------------")
        print(nickname)
-       print(session['nombre'])
+       print(session['name'])
        
        return redirect("/") 
 
@@ -51,3 +51,34 @@ def logout():
     # Remove the current user from the lists of users
     session.clear()
     return redirect("chat")
+
+#Socketio
+@socketio.on('connect')
+def channel_list():
+    emit('all-channels', channels=[])
+
+@socketio.on('newChannel')
+def create_channel(data):
+    global channels
+    channel = data[""]
+    # Identified if the new Channel already exitst 
+    if channel and "" not in channels:
+        channels[channel] = []
+        # Emit on the client-side
+        emit('newChannel', channel, broadcast=True)
+    else:
+        print(f'{channel}: name channel already taken')
+
+
+@socketio.on('messages')
+def newMessages(data):
+    global channels
+    channel = data["channel"]
+    message = {
+        "author": data["nickname"],
+        "message": data["message"],
+        "timestamp": strftime('%H', localtime())
+    }
+
+    if __name__=="__main__":
+        socketio.run(app)
--- static/scripts/socketio.js
+++ static/scripts/socketio.js
@@ -2,47 +2,43 @@ alert ('Conected');
 document.addEventListener('DOMContentLoaded', () => {
 
     var onstorage = window.localStorage;
+
+    var nickname = localStorage.getItem('nickname');
+    var currentChannel = localStorage.getItem('currentChannel')
+    
     var debugChannel = function() {
         console.log = ("var nickname" + nickname);
         console.log = ("var channel" + channel);
         console.log = ("onstorage.currentChannel" + onstorage.currentChanel);
-        // console.log = ("onstorage.privateRoom" + onstorage.privateRoom);
     };
+
+    debugChannel();
+
 
     // Websocket
     // Server-User connection
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
     socket.on('connect', function () {
+        
         const nickname = localStorage.getItem('nickname')
         console.log("Connected")
 
         socket.emit('online', {
             'user': nickname
         });
-
-        //Local Storage on current channel
-        if (storage.currentChannel){
-            if (onstorage.privateRoom === "false"){
-                joinChannel(onstorage.currentChannel);
-                channel = onstorage.currentChannel;
-            } else if (onstorage.privateRoom === "true"){
-                joinPrivateRoom(onstorage.currentChannel);
-                channel = onstorage.currentChannel;
-            } else {
-                // Default channel #general
-                joinChannel(channel);
-            }
-        } 
-        socket.on('message', function(msg) {
-            // Insert Content 
-            $('#texto-msj').append('<li>' + msg + '</li>')
-          })
-            //Send message
-          document.querySelector('#send-msg').onclick = () => {
-            console.log(document.querySelector("#user-msg").value);
-            socket.send(('#mi-msj').val());
-            $('#mi-msj').val('');
-          }
     });
+
+
+    // Send Message
+    document.querySelector("#message").onclick = () =>{
+        console.log(document.querySelector("user_message").value);
+
+        // Display messages to general channel
+        let data = {'message': document.querySelector("user_message").value, 'username':nickname, 'timestamp':formatTimestamp()};
+        formatMessage(data);
+    }
+    
+    // Clear the box chat
+    document.querySelector("user_message").value = "";
 
 });
